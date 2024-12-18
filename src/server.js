6ae40c206acc45f3418f303f73952714d2c850d3
unknown <vrnsingh60@gmail.com>
2024-12-18 11:22:08 +0530

    Server.js file updated

--- src/server.js
+++ src/server.js
@@ -1,20 +1,13 @@
-require("dotenv").config();
-const express = require("express");
 const path = require("path");
 
-const app = express();
-const PORT = process.env.PORT || 4000;
-const HOSTNAME = process.env.HOSTNAME;
-
-// Serve static files (CSS, JS, images)
-app.use(express.static(path.join(__dirname)));
-
-// Route to serve the index.html
-app.get("/weatherTracker", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
-});
-
-// Start the server
-app.listen(PORT, () => {
-  console.log(`Server is running on http://${HOSTNAME}:${PORT}`);
-});
+exports.handler = async (event, context) => {
+  if (event.path === "/weatherTracker") {
+    const htmlPath = path.join(__dirname, "../../index.html");
+    return {
+      statusCode: 200,
+      body: require("fs").readFileSync(htmlPath, "utf8"),
+      headers: { "Content-Type": "text/html" },
+    };
+  }
+  return { statusCode: 404, body: "Not Found" };
+};
