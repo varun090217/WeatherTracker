4d7d6879da94f45dc18930c6114b760780162672
unknown <vrnsingh60@gmail.com>
2024-12-18 11:14:14 +0530

    PORT updated

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 4000;
 const HOSTNAME = process.env.HOSTNAME;
 
 // Serve static files (CSS, JS, images)
