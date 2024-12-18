e1de92cbcd7587e1e9713f47fe623f1b0db17214
unknown <vrnsingh60@gmail.com>
2024-12-18 11:41:12 +0530

    server.js file path changed

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 exports.handler = async (event, context) => {
   if (event.path === "/weatherTracker") {
-    const htmlPath = path.join(__dirname, "../../index.html");
+    const htmlPath = path.join(__dirname, "../../src/index.html");
     return {
       statusCode: 200,
       body: require("fs").readFileSync(htmlPath, "utf8"),
