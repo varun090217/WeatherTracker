16ba273e5bd00f1fb167f773582dfbd66cf021b5
unknown <vrnsingh60@gmail.com>
2024-12-19 11:57:53 +0530

    app.js updated

--- app.js
+++ app.js
@@ -8,7 +8,11 @@ const PORT = process.env.PORT || 4000;
 app.use(express.static(path.join(__dirname, "src")));
 
 app.get("/weatherTracker", (req, res) => {
+  res.end("You are on web page");
   res.sendFile(path.join(__dirname, "src", "index.html"));
+});
+app.get("/", (req, res) => {
+  res.end("You are on Home Page");
 });
 
 app.listen(PORT);
